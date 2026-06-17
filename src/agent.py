ff0b7ab9abcc221fde3b710a74b4f21bfc441244
cutiepie-10 <jay.aroracs@gmail.com>
2026-06-17 13:08:06 +0530

    added prompt compressor ad corrected return type

--- src/agent.py
+++ src/agent.py
@@ -1,27 +1,56 @@
-import json
+import os
+import logging
 from llama_index.tools.duckduckgo import DuckDuckGoSearchToolSpec
 from llama_index.tools.wikipedia import WikipediaToolSpec
 from llama_index.llms.groq import Groq
 from llama_index.core.agent.workflow import ReActAgent
 from llama_index.core.prompts import RichPromptTemplate
-from src.config import MODEL, MAX_TOKENS, GROQ_API_KEY
+from workflows.retry_policy import (
+    retry_policy,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
 
-async def start_agent(feed:dict)->dict:
-    llm= Groq(
-        model = MODEL,
-        max_tokens = MAX_TOKENS,
-        temperature= 0.1,
-        api_key = GROQ_API_KEY
+
+
+from src.config import MODEL, MAX_TOKENS, GROQ_API_KEY, COMPRESSOR
+
+
+logger = logging.getLogger(__name__)
+
+
+async def start_agent(feed: dict) -> str:
+    llm = Groq(
+        model=MODEL,
+        max_tokens=MAX_TOKENS,
+        temperature=0.1,
+        api_key=GROQ_API_KEY
     )
-    tools=[]
-    tools.append(WikipediaToolSpec.to_tool_list())
-    tools.append(DuckDuckGoSearchToolSpec.to_tool_list())
-    agent = ReActAgent.from_tools(
-        tools = tools,
-        llm = llm,
-        verbose = True
+    policy = retry_policy(
+        retry=retry_if_exception_type((TimeoutError, ConnectionError)),
+        wait=wait_exponential(multiplier=1, exp_base=2, max=30),
+        stop=stop_after_attempt(2),
     )
-    template_str= """
+
+    tools = []
+    for tool in WikipediaToolSpec().to_tool_list():
+        tools.append(tool)
+    for tool in DuckDuckGoSearchToolSpec().to_tool_list():
+        tools.append(tool)
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    system_prompt_path = os.path.join(current_dir, "system_prompt.txt")
+    with open(system_prompt_path, "r", encoding='utf-8') as file:
+        system_prompt = file.read()
+    agent = ReActAgent(
+        tools=tools,
+        llm=llm,
+        verbose=True,
+        system_prompt=system_prompt,
+        retry_policy=policy,
+    )
+    template_str = """
     Providing the input information for the filing or the news
     ---------
         headline: {{headline}}
@@ -30,17 +59,28 @@ async def start_agent(feed:dict)->dict:
         category:{{category}}
         ticker_tags:{{ticker_tags}}
     ---------
-    Now based on the input above tell the sentiment, urgency score, is market moving, catalyst type and catalyst summary.
+    Now based on the input above tell the sentiment, urgency score, is market moving, ticker_tags, catalyst type and catalyst summary.
     """
+    
+    if len(feed["parsed_text"]) > 500:
+        compressed_prompt = COMPRESSOR.compress_prompt_llmlingua2(
+            feed["parsed_text"],
+            rate=0.4,
+        )
+        feed["parsed_text"] = compressed_prompt['compressed_prompt']
+        logger.info("Compressed prompt to %d from %d", len(
+            compressed_prompt['compressed_prompt']), len(prompt))
     template = RichPromptTemplate(template_str)
-    prompt = template.format_messages(headline= feed['headline'] or feed["subject"] , summary= feed["summary"], parsed_text= feed["parsed_text"], 
-    ticker_tags=feed["ticker_tags"] or feed["symbol"], category= feed["category"] or feed["filing_type"])
-    with open("system_prompt.txt","r",encoding= 'utf-8') as file:
-        system_prompt= file.read()
-    agent.update_prompts({"react_header":system_prompt})
-    response= await agent.run(
-        prompt, max_iterations=5, early_stopping_method= "generate"
+    prompt = template.format(
+        headline=feed['headline'] or feed["subject"],
+        summary=feed["summary"], parsed_text=feed["parsed_text"], ticker_tags=feed.get(
+            "ticker_tags")
+        or feed.get("symbol"), category=feed.get("category") or feed.get("filing_type")
     )
-    res = json.loads(response)
-    print(res)
+    response = await agent.run(
+        compressed_prompt['compressed_prompt'], max_iterations=5, early_stopping_method="generate"
+    )
+    res = response.response
+    logger.info("Final answer is produced of %d tokens ",
+                len(res))
     return res
