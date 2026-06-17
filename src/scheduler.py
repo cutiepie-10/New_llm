90802ccaae4ead9bca40a891a29db4dbf8765a61
cutiepie-10 <jay.aroracs@gmail.com>
2026-06-17 19:44:10 +0530

    corrected the get insight function logic

--- src/scheduler.py
+++ src/scheduler.py
@@ -7,7 +7,7 @@ from apscheduler.triggers.interval import IntervalTrigger
 
 from src.db import fetch_unprocessed_news, fetch_unprocessed_nse, upsert_news_insight
 from src.agent import start_agent
-from src.helpers import pdf_reader, webpage_reader,parse_insight
+from src.helpers import pdf_reader, webpage_reader, parse_insight, compress_article
 from src.config import INSIGHT_REFRESH_TIME
 
 
@@ -15,25 +15,37 @@ logger = logging.getLogger(__name__)
 
 
 def get_insights():
+    """
+    Fetches the unprocessed news, parses the url/pdf and then compresses. 
+    Finally get the news insight from the agent.
+    """
     raw_news = fetch_unprocessed_news()
     for news in raw_news:
-        news["parsed_text"] = webpage_reader(news["url"])
+        news["parsed_text"] = compress_article(webpage_reader(news["url"]))
         insight = asyncio.run(start_agent(news))
-        asyncio.sleep(delay= 1000)
-        parsed= parse_insight(insight)
+        parsed = parse_insight(insight)
         parsed["raw_news_id"] = news["id"]
-        upsert_news_insight(parsed, "rss")
+        parsed['body'] = news['parsed_text']
+        parsed['raw_response'] = insight
+        count = upsert_news_insight(parsed, "rss")
+        logger.info(
+            "Inserted %d rows in the news_insight table successfully", count)
     nse_filings = fetch_unprocessed_nse()
     for nse in nse_filings:
-        nse["parsed_text"] = pdf_reader(nse["pdf_url"])
+        nse["parsed_text"] = compress_article(pdf_reader(nse["pdf_url"]))
         insight = asyncio.run(start_agent(nse))
-        asyncio.sleep(delay= 1000)
-        parsed= parse_insight(insight)
+        parsed = parse_insight(insight)
         parsed["nse_filing_id"] = nse["filing_id"]
-        upsert_news_insight(parsed, "nse")
+        parsed['raw_response'] = insight
+        count = upsert_news_insight(parsed, "nse")
+        logger.info(
+            "Inserted %d rows in the news_insight table successfully", count)
 
 
 def start():
+    """
+    Schedules the get insight function to start the agent.
+    """
     scheduler = BlockingScheduler(timezone='Asia/Kolkata')
     scheduler.add_job(
         get_insights,
