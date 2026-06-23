9753e8351e2ff48df7d109c3f8653fcec66de48d
cutiepie-10 <jay.aroracs@gmail.com>
2026-06-23 20:10:02 +0530

    added test for upsert function

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 
-from src.db import fetch_unprocessed_news, fetch_unprocessed_nse
+from src.db import fetch_unprocessed_news, fetch_unprocessed_nse, upsert_news_insight
 
 
 py_test_mark = pytest.mark.skipif(
@@ -9,7 +9,17 @@ py_test_mark = pytest.mark.skipif(
     reason="DATABASE_URL not in environment",
 )
 
-
+INSIGHT = {
+    'raw_news_id':'05bf3f4f-e6f6-4573-8908-38f743a80592',
+    'ticker_tags':[],
+    'sentiment':'bearish',
+    'urgency_score':2,
+    'catalyst_type':'other',
+    'catalyst_summary':'data',
+    'is_market_moving':False,
+    'raw_response':"",
+    'nse_filing_id':'2e1e1e241411-1245124dsf-e332df2f23'
+}
 def test_fetch_unprocessed_news():
     """
     Tests the news fetch function
@@ -28,4 +38,8 @@ def test_fetch_unprocessed_nse():
     assert len(nse) > 5
 
 def test_upsert_insight():
-    
+    """
+    Tests whether the upsert insight function works
+    """
+    assert upsert_news_insight(INSIGHT,'rss')>1
+    assert upsert_news_insight(INSIGHT,'nse')>1
